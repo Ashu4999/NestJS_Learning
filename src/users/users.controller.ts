d11568419f5993de7ce27e4fe94d9a303ff97937
Ashu4999 <ashutoshgorke77@gmail.com>
2024-08-07 12:36:13 +0530

    lesson complete

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,32 +1,32 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+    constructor(private readonly usersService: UsersService) {}
 
     @Get() //GET /users
     findAll(@Query('role') role?: "INTERN" | "ADMIN" | "ENGINEER") {
-        return "In users get";
+        return this.usersService.findAll();
     }
 
     @Get(":id") //GET /users/:id 
     findOne(@Param("id") id: string) {
-        return { id }
+        return this.usersService.findOne(parseInt(id));
     }
 
     @Post() //Post /users
-    create(@Body() user: {}) {
-        console.log(user);
-        return user;
+    create(@Body() user: { name: string, email: string, role: 'INTERN' | 'ENGINEER' | 'ADMIN' }) {
+        return this.usersService.create(user);
     }
 
-
     @Patch(":id") //UPDATE /users/:id 
-    update(@Param("id") id: string, @Body() userUpdate: {}) {
-        return { id, ...userUpdate };
+    update(@Param("id") id: string, @Body() userUpdate: { name?: string, email?: string, role?: 'INTERN' | 'ENGINEER' | 'ADMIN' }) {
+        return this.usersService.update(parseInt(id), userUpdate);
     }
 
     @Delete(":id") //DELETE /users/:id
     delete(@Param("id") id: string) {
-        return { message: `User ${id} deleted successfully.` };
+        return this.usersService.delete(parseInt(id));
     }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,80 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    private users = [
+        {
+            "id": 1,
+            "name": "Leanne Graham",
+            "email": "Sincere@april.biz",
+            "role": "INTERN",
+        },
+        {
+            "id": 2,
+            "name": "Ervin Howell",
+            "email": "Shanna@melissa.tv",
+            "role": "INTERN",
+        },
+        {
+            "id": 3,
+            "name": "Clementine Bauch",
+            "email": "Nathan@yesenia.net",
+            "role": "ENGINEER",
+        },
+        {
+            "id": 4,
+            "name": "Patricia Lebsack",
+            "email": "Julianne.OConner@kory.org",
+            "role": "ENGINEER",
+        },
+        {
+            "id": 5,
+            "name": "Chelsey Dietrich",
+            "email": "Lucio_Hettinger@annie.ca",
+            "role": "ADMIN",
+        }
+    ];
+
+    findAll(role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+        if (role) {
+            return this.users.filter(user => user.role === role)
+        }
+        return this.users
+    }
+
+    findOne(id: number) {
+        const user = this.users.find(user => user.id === id)
+        if (!user) {
+            throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+        }
+        return user
+    }
+
+    create(user: { name: string, email: string, role: 'INTERN' | 'ENGINEER' | 'ADMIN' }) {
+        const sortedByID = [...this.users].sort((a, b) => a.id - b.id)
+        const newUser = {
+            id: sortedByID[sortedByID.length - 1].id + 1,
+            ...user
+        }
+        this.users.push(newUser)
+        return newUser
+    }
+
+    update(id: number, updatedUser: { name?: string, email?: string, role?: 'INTERN' | 'ENGINEER' | 'ADMIN' }) {
+        let updateUserIndex = this.users.findIndex(user => user.id === id);
+        if (updateUserIndex === -1) {
+            throw new HttpException('User not found to update', HttpStatus.NOT_FOUND);
+        }
+        this.users[updateUserIndex] = { ...this.users[updateUserIndex], ...updatedUser };
+        return this.findOne(id);
+    }
+
+    delete(id: number) {
+        const removedUser = this.findOne(id)
+        if (removedUser) {
+            throw new HttpException('User not found to delete', HttpStatus.NOT_FOUND);
+        }
+        this.users = this.users.filter(user => user.id !== id)
+        return removedUser
+    }
+}
